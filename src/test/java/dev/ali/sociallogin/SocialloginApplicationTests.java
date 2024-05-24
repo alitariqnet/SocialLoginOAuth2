c3f6089029067f8c1f80e154949abd0574352fde
Ali Tariq <alibabapk@aol.com>
2024-05-24 14:10:31 +0500

    Initial commit

---
+++ src/test/java/dev/ali/sociallogin/SocialloginApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ali.sociallogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
